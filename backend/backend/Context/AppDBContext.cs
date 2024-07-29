a6712915e475b260c639dba0e2b418c2c688f44f
Gabriel Machado dos Santos <santos.gabriel67018@gmail.com>
2024-07-29 00:03:50 -0300

    :sparkles: feat: Construindo a estrutura do Servidor (back-end).

---
+++ backend/backend/Context/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using SGED.Context.Builders;
+//using SGED.Objects.Models.Entities;
+
+namespace SGED.Context;
+public class AppDBContext : DbContext
+{
+	public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+    // Mapeamento Relacional dos Objetos no Bando de Dados
+
+
+    // Fluent API
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        
+    }
+}
